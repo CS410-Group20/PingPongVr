17a48a2895b213dcc0bcd5daba36c2832d88c216
Pargev Soghoyan <soghoyanpargev@gmail.com>
2021-01-24 13:24:06 +0400

    [ADD] initial commit

---
+++ Kursayin/Assets/Scripts/GunController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    [SerializeField] private Transform _ball;
+    [SerializeField] private Transform _target;
+    [SerializeField] private float forceWeight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Shoot());
+    }
+
+    IEnumerator Shoot()
+    {
+        while (true)
+        {
+            Rigidbody ball = Instantiate(_ball, _target.position, Quaternion.identity).GetComponent<Rigidbody>();
+            ball.velocity = _target.forward.normalized * forceWeight;
+            yield return new WaitForSeconds(2);
+        }
+    }
+}
---
+++ Kursayin/Assets/Scripts/RocketController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketController : MonoBehaviour
+{
+    [SerializeField] private Transform _rocket;
+    [SerializeField] private float _axisXWeight;
+    [SerializeField] private float _axisYWeight;
+    [SerializeField] private float _axisZWeight;
+    [SerializeField] private float _hitForceWeight;
+    
+    
+    private float x = 0;
+    private float y = 0;
+    private float z = 0;
+
+    private void Start()
+    {
+        
+    }
+
+    public void Update()
+    {
+         x += GetAxisWeight("Mouse X", _axisXWeight);
+         y += GetAxisWeight("Mouse Y", _axisYWeight);
+         z += GetAxisWeight("Mouse Y", _axisZWeight);
+         _rocket.position = new Vector3(x, y+_rocket.position.y, z);
+         _rocket.rotation = Quaternion.Euler(0, 0,GetAngle());
+         
+    }
+
+    private float GetAngle()
+    {
+        var direction = Vector3.down - _rocket.position;
+        var directionProj = Vector3.ProjectOnPlane(direction, Vector3.forward);
+        var reflaction = Vector3.Reflect(direction,Vector3.up);
+        var reflactionProj = Vector3.ProjectOnPlane(Vector3.down-reflaction, Vector3.forward);
+
+        var angle = Vector3.SignedAngle(reflactionProj, directionProj,Vector3.forward);
+        return angle;
+    }
+    private float GetAxisWeight(string axisName, float axisWeight)
+    {
+        return Input.GetAxis(axisName) * axisWeight * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(GetAxisWeight("Mouse X", _axisXWeight),
+            0,
+            GetAxisWeight("Mouse Y", _axisZWeight))*_hitForceWeight;
+    }
+
+    
+}
